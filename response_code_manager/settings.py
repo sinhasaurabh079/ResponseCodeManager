3ce8049b4c7914e9332f651c5fc70232c5663280
Saurabh Sinha <sinhasaurabh079@gmail.com>
2024-11-27 20:53:51 +0530

    Update settings.py

--- response_code_manager/settings.py
+++ response_code_manager/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # ]
 STATIC_URL = 'static/'
 # settings.py
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
