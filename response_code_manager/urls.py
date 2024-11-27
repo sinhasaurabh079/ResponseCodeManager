2c28a8000c0a25c0b4842457b8bb0ca7ea5b1257
Saurabh Sinha <sinhasaurabh079@gmail.com>
2024-11-27 21:25:09 +0530

    Update urls.py

--- response_code_manager/urls.py
+++ response_code_manager/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from response_code_manager import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
